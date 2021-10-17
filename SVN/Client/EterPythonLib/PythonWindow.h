5f5f1f13700f7b403211560ed792338d657e8421
Licymnius <licymnius@outlook.com>
2021-10-17 09:49:30 +0300

    Update PythonWindow.h

--- SVN/Client/EterPythonLib/PythonWindow.h
+++ SVN/Client/EterPythonLib/PythonWindow.h
@@ -1,20 +1,20 @@
 //find;
 
-			virtual BOOL	OnMouseMiddleButtonUp();
+		virtual BOOL	OnMouseMiddleButtonUp();
 
 //add below;
 
 #ifdef ENABLE_MOUSEWHEEL_EVENT
-			virtual BOOL	OnMouseWheelScroll(short wDelta);
-			virtual void	SetScrollable();
+		virtual BOOL	OnMouseWheelScroll(short wDelta);
+		virtual void	SetScrollable();
 #endif
 
 //find again;
 
-			TWindowContainer	m_pReserveChildList;
+		TWindowContainer	m_pReserveChildList;
 
 //add below;
 
 #ifdef ENABLE_MOUSEWHEEL_EVENT
-			bool				m_bIsScrollable;
+		bool				m_bIsScrollable;
 #endif
