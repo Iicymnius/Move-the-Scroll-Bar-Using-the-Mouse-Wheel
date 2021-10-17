fd25aa758c6927f3d42d95a6790c2d9f0e0efc74
Licymnius <licymnius@outlook.com>
2021-10-17 09:50:13 +0300

    Update PythonWindowManager.h

--- SVN/Client/EterPythonLib/PythonWindowManager.h
+++ SVN/Client/EterPythonLib/PythonWindowManager.h
@@ -1,9 +1,9 @@
 //find;
 
-			void		RunMouseMiddleButtonUp(long x, long y);
+		void		RunMouseMiddleButtonUp(long x, long y);
 
 //add below;
 
 #ifdef ENABLE_MOUSEWHEEL_EVENT
-			bool		RunMouseWheelScroll(long x, long y , short wDelta);
+		bool		RunMouseWheelScroll(long x, long y , short wDelta);
 #endif
