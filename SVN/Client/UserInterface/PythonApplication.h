abd52ff1990bc81eb4492700cf3c451bddac4195
Licymnius <licymnius@outlook.com>
2021-10-17 09:51:25 +0300

    Update PythonApplication.h

--- SVN/Client/UserInterface/PythonApplication.h
+++ SVN/Client/UserInterface/PythonApplication.h
@@ -1,9 +1,9 @@
 //find;
 
-		void OnMouseRightButtonUp(int x, int y);
+	void OnMouseRightButtonUp(int x, int y);
 
 //add below;
 
 #ifdef ENABLE_MOUSEWHEEL_EVENT
-		bool OnMouseWheelScroll(long x, long y , short wDelta);
+	bool OnMouseWheelScroll(long x, long y , short wDelta);
 #endif
